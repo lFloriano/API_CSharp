a58d8b899168ae7de26f89c6e6a395c88fa491b8
=lFloriano <lwcas.franca@gmail.com>
2019-11-28 21:46:01 -0300

    COmmit inicial

---
+++ API1/API1/Controllers/UsuarioController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API1.Models;
+
+namespace API1.Controllers
+{
+    [RoutePrefix("api/usuario")]
+    public class UsuarioController : ApiController
+    {
+        private static List<UsuarioModel> listaUsuarios = new List<UsuarioModel>();
+
+        [HttpPost]
+        [Route("CadastrarUsuario")]
+        public string CadastrarUsuario(UsuarioModel usuario)
+        {
+            listaUsuarios.Add(usuario);
+            return "Usuário cadastrado com sucesso!";
+        }
+
+        [HttpPut]
+        [Route("AlterarUsuario")]
+        public string AlterarUsuario(UsuarioModel usuario)
+        {
+            listaUsuarios
+                .Where(x => x.Codigo == usuario.Codigo)
+                .Select(x =>
+                    {
+                        x.Codigo = usuario.Codigo;
+                        x.Nome = usuario.Nome;
+                        x.Login = usuario.Login;
+
+                        return x;
+                    }).ToList();
+
+            return "Usuário alterado com sucesso!";
+        }
+
+        [HttpDelete]
+        [Route("ExcluirUsuario/{codigo}")]
+        public string ExcluirUsuario(int codigo)
+        {
+            UsuarioModel usuario = listaUsuarios.Where(x => x.Codigo == codigo)
+                                                .Select(x => x)
+                                                .First();
+            listaUsuarios.Remove(usuario);
+
+            return "Registro excluído com sucesso!";
+        }
+
+        [HttpGet]
+        [Route("ConsultarUsuarios")]
+        public List<UsuarioModel> ConsultarUsuarios()
+        {
+            return listaUsuarios;
+        }
+
+    }
+}
---
+++ API1/API1/Models/UsuarioModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API1.Models
+{
+    public class UsuarioModel
+    {
+        private int codigo;
+        private string nome;
+        private string login;
+
+        public UsuarioModel() { }
+
+        public UsuarioModel(int codigo, string nome, string login)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Login = login;
+        }
+
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+
+    }
+}
